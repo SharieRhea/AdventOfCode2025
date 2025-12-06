d64d6b8fc1087bf3acfa4d1645e6ef3a82b4b635
SharieRhea <sharierhea@gmail.com>
2025-12-06 15:43:02 -0500

    shorten main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,13 @@
 open AdventOfCode2025
 
+let days = [ Day01.run; Day02.run; Day03.run; Day04.run; Day05.run ]
+
 let () =
   print_endline "Enter a day to run or 0 for all days: ";
-  let day = read_int () in
-  match day with
-  | 0 ->
-      Day01.run ();
-      Day02.run ();
-      Day03.run ();
-      Day04.run ();
-      Day05.run ()
-  | 1 -> Day01.run ()
-  | 2 -> Day02.run ()
-  | 3 -> Day03.run ()
-  | 4 -> Day04.run ()
-  | 5 -> Day05.run ()
-  | _ -> print_endline "Invalid entry"
+  let input = read_int () in
+  match input with
+  | 0 -> List.iter (fun day -> day ()) days
+  | _ -> (
+      match List.nth_opt days (input - 1) with
+      | Some day -> day ()
+      | None -> failwith "Invalid day!")
