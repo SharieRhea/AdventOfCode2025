7e92f0474f84afd329cfeb16fd17175304ee8e5a
SharieRhea <sharierhea@gmail.com>
2025-12-08 11:38:18 -0500

    add day 7 part 2, clean up

--- lib/day07.ml
+++ lib/day07.ml
@@ -1,5 +1,3 @@
-type node = { position : int * int; id : int }
-
 module IntPair = struct
   type t = int * int
 
@@ -8,17 +6,6 @@ module IntPair = struct
 end
 
 module IntPairSet = Set.Make (IntPair)
-
-module Node = struct
-  type t = node
-
-  let compare node1 node2 =
-    match compare node1.position node2.position with
-    | 0 -> compare node1.id node2.id
-    | c -> c
-end
-
-module NodeSet = Set.Make (Node)
 
 (* return a set of (x, y) coordinates for '^' tiles and the starting tile coordinates *)
 let parse_splitters lines =
@@ -43,51 +30,51 @@ let parse_splitters lines =
   let start = List.find (fun (_, character) -> character = 'S') tiles in
   (splitters, fst start)
 
-let trace_beam splitters start max_height =
-  let queue = Queue.create () in
-  Queue.push start queue;
+(* add beams to the table, accounting for beams already on that tile *)
+let add_beams table key beams =
+  match Hashtbl.find_opt table key with
+  | None -> Hashtbl.add table key beams
+  | Some existing -> Hashtbl.replace table key (existing + beams)
 
-  (* imperative queue :( *)
-  let rec trace activated_splitters visited : int =
-    if Queue.is_empty queue then
-      (IntPairSet.cardinal activated_splitters)
-    else
-      let x, y = Queue.pop queue in
-      (* if we've passed all possible splitters, this beam no longer matters *)
-      if y > max_height then trace activated_splitters visited
-        (* encountering a splitter, create two new positions to follow *)
-      else if IntPairSet.mem (x, y + 1) splitters then
-        let new_splitters = IntPairSet.add (x, y + 1) activated_splitters in
-        match ((x - 1, y + 1), (x + 1, y + 1)) with
-        | left, right
-          when (not (IntPairSet.mem left visited))
-               && not (IntPairSet.mem right visited) ->
-            Queue.push left queue;
-            Queue.push right queue;
-            trace new_splitters
-              (visited |> IntPairSet.add left |> IntPairSet.add right)
-        | left, _ when not @@ IntPairSet.mem left visited ->
-            Queue.push left queue;
-            trace new_splitters (visited |> IntPairSet.add left)
-        | _, right when not @@ IntPairSet.mem right visited ->
-            Queue.push right queue;
-            trace new_splitters (visited |> IntPairSet.add right)
-        | _ -> trace new_splitters visited
-      (* just empty space, continue moving *)
-        else (
-        Queue.push (x, y + 1) queue;
-        let visited = IntPairSet.add (x, y + 1) visited in
-        trace activated_splitters visited)
+let trace_beam splitters start max_x max_y : int * int =
+  let table = Hashtbl.create 32 in
+  let activated = ref IntPairSet.empty in
+  Hashtbl.add table start 1;
+
+  (* imperative, i'm sorry :( *)
+  for y = 0 to max_y - 1 do
+    for x = 0 to max_x - 1 do
+      match Hashtbl.find_opt table (x, y - 1) with
+      | None -> () (* there were no beams above, do nothing *)
+      | Some incoming ->
+          if IntPairSet.mem (x, y) splitters then (
+            (* encountered splitter, add to set and move beams to either side *)
+            activated := IntPairSet.add (x, y) !activated;
+            let left, right = ((x - 1, y), (x + 1, y)) in
+            add_beams table left incoming;
+            add_beams table right incoming)
+          else
+            (* no splitter, just move beams down to this tile *)
+            add_beams table (x, y) incoming
+    done
+  done;
+
+  (* sum all the beams on the last row to find the number of timelines *)
+  let timelines =
+    Hashtbl.fold
+      (fun (_, y) beams sum -> if y = max_y - 1 then sum + beams else sum)
+      table 0
   in
-  let empty_visited = IntPairSet.of_list [ start ] in
-  trace IntPairSet.empty empty_visited
+  (!activated |> IntPairSet.cardinal, timelines)
 
 let run () =
-  print_endline "DAY 5:";
-  let lines = In_channel.input_lines (open_in "resources/test.txt") in
-  let splitters, start = parse_splitters lines in
-  let max_height = List.length lines in
+  print_endline "DAY 7:";
+  let lines = In_channel.input_lines (open_in "resources/day07.txt") in
+  let max_x, max_y =
+    (lines |> List.hd |> String.length, lines |> List.length)
+  in
 
-  let activated_splitters = trace_beam splitters start max_height in
-  print_endline ("    Part 1: " ^ string_of_int activated_splitters);
-  print_endline ("    Part 2: " ^ string_of_int 0)
+  let splitters, start = parse_splitters lines in
+  let activated, timelines = trace_beam splitters start max_x max_y in
+  print_endline ("    Part 1: " ^ string_of_int activated);
+  print_endline ("    Part 2: " ^ string_of_int timelines)
